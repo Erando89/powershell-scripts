4f69f0a3bea669aea2e5fb91f1dabf34079d4c9b
Erando89 <a.mickley1@gmx.de>
2018-11-28 19:05:37 +0100

    split.ps1 init

---
+++ split/split.ps1
@@ -0,0 +1,33 @@
+$json = Get-Content -Raw -Path tracks.json | ConvertFrom-Json
+$tracks = $json.tracks
+#$firstMediaFile = dir .\* -include ('*.mp4', '*.avi') -recurse
+$largestFile = gci | sort Length -desc
+$sourceFile = $largestFile[0]
+$sourceFileExtension = dir .\* -include ('*.mp4', '*.avi') -recurse | select BaseName,Extension
+$sourceFileExtension = $sourceFileExtension[0].Extension
+#Write-Output ($tracks.Count)
+for($i = 0; $i -lt $tracks.Count; $i++)
+{
+	$track = $tracks[$i]
+	Write-Output ($track.name + '//' + $track.start)
+	
+	if ($i + 1 -lt $tracks.Count){
+	
+		$StartDate=[datetime]("01/01/2018 " + $track.start)
+		$EndDate=[datetime]("01/01/2018 " + $tracks[$i+1].start)
+		$duration = NEW-TIMESPAN -Start $StartDate -End $EndDate
+		$durationString = "" + $duration.Hours.ToString("00") + ":" + $duration.Minutes.ToString("00") + ":" + $duration.Seconds.ToString("00") + "." + $duration.Milliseconds.ToString("00")
+	}
+	
+	$out = $json.artist + " - " + ($i+1).ToString("00") + " - " + $track.name+$sourceFileExtension
+	$metaArtist = "" + $json.artist
+	$metaName = "" + $track.name
+	$metaYear = "" + $json.year
+	$metaTrack = "" + ($i+1)
+	if ($durationString) {
+		ffmpeg -y -ss $track.start -i $sourceFile.Name -metadata year=$metaYear -metadata artist=$metaArtist -metadata title=$metaName -metadata track=$metaTrack -c copy -t $durationString $out
+	} else {
+		ffmpeg -y -ss $track.start -i $sourceFile.Name -metadata year=$metaYear -metadata artist=$metaArtist -metadata title=$metaName -metadata track=$metaTrack -c copy $out
+	}
+
+}
