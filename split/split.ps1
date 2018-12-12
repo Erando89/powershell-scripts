36d1a12b1f58834749650bb1485472e3ac98053a
Erando89 <a.mickley1@gmx.de>
2018-12-12 21:15:05 +0100

    skip function
    The script now will skip a track when it's name consists of 'x's only.

--- split/split.ps1
+++ split/split.ps1
@@ -6,6 +6,7 @@ $sourceFile = $largestFile[0]
 $sourceFileExtension = dir .\* -include ('*.mp4', '*.avi') -recurse | select BaseName,Extension
 $sourceFileExtension = $sourceFileExtension[0].Extension
 #Write-Output ($tracks.Count)
+$realtrackcount = 0
 for($i = 0; $i -lt $tracks.Count; $i++)
 {
 	$track = $tracks[$i]
@@ -20,14 +21,19 @@ for($i = 0; $i -lt $tracks.Count; $i++)
 		$durationString = "" + $duration.Hours.ToString("00") + ":" + $duration.Minutes.ToString("00") + ":" + $duration.Seconds.ToString("00") + "." + $duration.Milliseconds.ToString("00")
 	}
 	
-	$out = $json.artist + " - " + ($i+1).ToString("00") + " - " + $track.name+$sourceFileExtension
+	# check if track name consists only of 'x' -> skip in numeration
+	$skipTitle = $track.name.replace('x','').length -eq 0
+	if (!$skipTitle) {
+		$realtrackcount++
+	}
+	$out = $json.artist + " - " + ($realtrackcount).ToString("00") + " - " + $track.name+$sourceFileExtension
 	$metaArtist = "" + $json.artist
 	$metaName = "" + $track.name
 	$metaYear = "" + $json.year
-	$metaTrack = "" + ($i+1)
-	if ($durationString) {
+	$metaTrack = "" + ($realtrackcount)
+	if ($durationString -and !$skipTitle) {
 		ffmpeg -y -ss $track.start -i $sourceFile.Name -metadata year=$metaYear -metadata artist=$metaArtist -metadata title=$metaName -metadata track=$metaTrack -c copy -t $durationString $out
-	} else {
+	} elseif (!$skipTitle) {
 		ffmpeg -y -ss $track.start -i $sourceFile.Name -metadata year=$metaYear -metadata artist=$metaArtist -metadata title=$metaName -metadata track=$metaTrack -c copy $out
 	}
 
